e1006602c29da72f1ce6f05497507d7e0367e3b7
MinHongSun <alsghdtjs7@gmail.com>
2022-06-29 15:30:38 +0900

    modify

---
+++ BACKJOON_7568.py
@@ -0,0 +1,14 @@
+n = int(input())
+li = []
+result =[]
+for i in range(n):
+    li.append(list(map(int,input().split())))
+for i in range(n):
+    cnt = 1
+    for j in range(n):
+        if li[i][0] < li[j][0] and li[i][1] < li[j][1]:
+            cnt += 1
+    result.append(cnt)
+
+for k in result:
+    print(k, end=' ')
